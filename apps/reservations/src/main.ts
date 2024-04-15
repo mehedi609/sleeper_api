4daba5fc23c9a79ac213fd10045f3de928385a76
mehedi_hasan <mehedi609@gmail.com>
2024-04-15 15:13:01 +0900

    dockerized reservation app

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(5000);
+  await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
